e263c7d3d60d158d9e1ccd547e8fcb225d52d607
Anthony LaRocca <alarocca@apixio.com>
2025-01-29 09:44:22 -0500

    Improved formatting

--- src/uc_utils.cpp
+++ src/uc_utils.cpp
@@ -39,7 +39,10 @@ LogicalType UCUtils::TypeToLogicalType(ClientContext &context, const string &typ
 		return LogicalType::INTEGER;
 	} else if (type_text == "long") {
 		return LogicalType::BIGINT;
-	} else if (type_text == "string" || type_text.find("varchar(") == 0|| type_text == "char"  || type_text.find("char(") == 0) {
+	} else if (type_text == "string" ||
+	           type_text.find("varchar(") == 0 ||
+	           type_text == "char" ||
+	           type_text.find("char(") == 0) {
 		return LogicalType::VARCHAR;
 	} else if (type_text == "double") {
 		return LogicalType::DOUBLE;
